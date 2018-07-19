1dd7f896b9460bd5f858dfb22f1c58acd0b807a6
Carmen Owen <doctorsout@yahoo.com>
2018-07-19 14:00:28 -0400

    using console.table and added more logic

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require('inquirer');
 var prompt = require('prompt');
+require('console.table')
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -16,13 +17,20 @@ connection.connect(function(err) {
       return;
     }; 
     console.log("connected as id " + connection.threadId);
+    loadProducts()
+    
+    //buyHowMany();
+    
+});
 
+function loadProducts(){
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) {
           throw err;
         };
 
-        console.log("------------------------------\n");
+        //JAVASCRIPT WAY / HARD WAY
+       /* console.log("------------------------------\n");
         console.log('Products: \n');
 
         for (i = 0; i < res.length; i++) {
@@ -30,18 +38,55 @@ connection.connect(function(err) {
             console.log(res[i].product_name);
             console.log(res[i].price);
             console.log("------------------------------\n");
-        };
+        };*/
+        console.table(res)
+        buyWhat(res);
     });
-    connection.end();
-});
+}
 
-function buyWhat() {
-    inquirer.prompt(["What do you want to buy?  Select the corresponding ID."]).then(answers => {
-        res[1].id
+function buyWhat(inv) {
+   
+    inquirer
+    .prompt([
+        {
+            type:"input",
+            name:"choice",
+            message:"What is the ID of the item you want to buy?"
+        },
+        {
+            type:"input",
+            name:"howMany",
+            message:"How many would you like??"
+        }
+    ])
+    .then(answers => {
+        console.log("ID: ", answers.choice)
+        console.log("QTY: ", answers.howMany)       
+
+        /*
+        [
+            {
+                id:1, 
+                product_name:"paper",
+                department_name:"stock room", 
+                price: 30, 
+                stock_quantity: 97
+            }
+        ]
+                */
+              
+
+        for (var i = 0; i < inv.length; i ++) {          
+            
+            if (answers.choice == inv[i].id && answers.howMany <= inv[i].stock_quantity) {
+                console.log("answers.choice = inv.id", inv[i].id);
+
+            }  
+        }
+        
     });
 };
 
-function buyHowMany() {
-    console.log("How many would you like?");
+function checkInventory(id,inventory){
 
-};
+}
