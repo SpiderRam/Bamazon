1e099cabd3112867a62ea27c3eec25b4547f0385
Carmen Owen <doctorsout@yahoo.com>
2018-07-23 17:34:07 -0400

    functions written, problem running inquirer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,12 @@
 var mysql = require("mysql");
 var inquirer = require('inquirer');
-var prompt = require('prompt');
-require('console.table')
+require('console.table');
+
+var stockQuantity;
+var newStockQuantity;
+var customerTotal;
+var selectedItem;
+var cost;
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -17,76 +22,96 @@ connection.connect(function(err) {
       return;
     }; 
     console.log("connected as id " + connection.threadId);
-    loadProducts()
-    
-    //buyHowMany();
-    
+    loadProducts();
 });
 
-function loadProducts(){
-    connection.query("SELECT * FROM products", function(err, res) {
+function loadProducts() {
+    connection.query("SELECT * FROM products", function(err,res) {
         if (err) {
-          throw err;
+            throw err;
         };
-
-        //JAVASCRIPT WAY / HARD WAY
-       /* console.log("------------------------------\n");
-        console.log('Products: \n');
-
-        for (i = 0; i < res.length; i++) {
-            console.log(res[i].id);
-            console.log(res[i].product_name);
-            console.log(res[i].price);
-            console.log("------------------------------\n");
-        };*/
-        console.table(res)
-        buyWhat(res);
+        console.table(res);
+        chooseItems();
     });
-}
-
-function buyWhat(inv) {
-   
-    inquirer
-    .prompt([
-        {
-            type:"input",
-            name:"choice",
-            message:"What is the ID of the item you want to buy?"
-        },
-        {
-            type:"input",
-            name:"howMany",
-            message:"How many would you like??"
-        }
-    ])
-    .then(answers => {
-        console.log("ID: ", answers.choice)
-        console.log("QTY: ", answers.howMany)       
-
-        /*
-        [
+};
+    
+function chooseItems() {
+    console.log("chooseItems called");
+    connection.query("SELECT * FROM products", function(err, results) {
+        if (err) {
+            throw err;
+        };
+    
+        inquirer
+        .prompt([
             {
-                id:1, 
-                product_name:"paper",
-                department_name:"stock room", 
-                price: 30, 
-                stock_quantity: 97
+                name: "choice",
+                type: "rawlist",
+                message: "What is the ID of the item you wish to buy?",
+                choices: function createItemArray() {
+                    var itemArray = [];
+                    for (var i = 0; i < results.length; i++) {
+                        itemArray.push(results[i].product_name);
+                    }
+                    return itemArray;
+                }
+            },
+            {
+                type: "input",
+                name: "quantity",
+                message: "How many of this item would you like to buy?"
             }
-        ]
-                */
-              
-
-        for (var i = 0; i < inv.length; i ++) {          
-            
-            if (answers.choice == inv[i].id && answers.howMany <= inv[i].stock_quantity) {
-                console.log("answers.choice = inv.id", inv[i].id);
-
-            }  
-        }
-        
+        ])
+        .then(
+            checkStockQuantity(),
+        )
     });
 };
 
-function checkInventory(id,inventory){
+function checkStockQuantity(answer) {
+    
+    for (var i = 0; i < results.length; i++) {
+        if (results[i].product_name === answer.choice) {
+            selectedItem = results[i];
+            stockQuantity = selectedItem.stock_quantity;
+            requestedQuantity = answer.quantity;
+            cost = selectedItem.price;
+        };
+    };
+    if (stockQuantity >= parseInt(requestedQuantity)) {
+        changeStockQuantity();
+        calculateTotal();
+        connection.query (
+            "UPDATE products SET ? WHERE ?",
+            [
+                {
+                    stock_quantity: newStockQuantity
+                },
+                {
+                    id: selectedItem.id
+                }
+            ],
+            function(error) {
+                if (error) {
+                    throw error;
+                } else{
+                    console.log("Your total is" + customerTotal);
+                    loadProducts();
+                }
+            }
+        )
+    } else {
+        console.log("Insufficient quantity, try again.");
+        loadProducts();
+    };
+};
 
-}
+function changeStockQuantity() {
+    newStockQuantity = stockQuantity - requestedQuantity;
+};
+
+function calculateTotal() {
+    customerTotal = cost * requestedQuantity;
+};
+
+
